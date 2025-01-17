c517f10dfc6d6bb35fb8dee3454fefda5127213d
Yakubovich17 <plmml0qazza161@gmail.com>
2025-01-17 11:24:03 +0000

    Included sqlite db

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("todolist.config.Config")
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- todolist/config.py
+++ todolist/config.py
@@ -1,2 +1,6 @@
+import os
+
 class Config:
-    pass
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), "db.sqlite3")}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
