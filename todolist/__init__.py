5d82256fa7b31d3b136e9046224d3fd9694b4ed8
Antorof1 <antorof@proton.me>
2025-01-18 17:46:35 +0100

    Added basic auth foundation

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from importlib import import_module
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("todolist.config.Config")
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     for module_name in ("tasks", "main", "auth"):
         module = import_module(f"todolist.{module_name}.routes")
@@ -16,5 +19,9 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    @app.teardown_request
+    def remove_session(exception=None):
+        db.session.remove()
 
     return app
