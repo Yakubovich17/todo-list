7ef1a6b8ba68afa830f01e40ffbf989bc627ae3f
Antorof1 <antorof@proton.me>
2025-01-18 18:41:47 +0100

    Added additional forms check

--- todolist/auth/forms.py
+++ todolist/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class SignInForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
@@ -8,9 +8,9 @@ class SignInForm(FlaskForm):
     submit = SubmitField("Sign In")
 
 class SignUpForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    email = EmailField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    username = StringField("Username", validators=[DataRequired(), Length(min=5, max=64)])
+    email = EmailField("Email", validators=[DataRequired(), Email(), Length(max=64)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password", message="Passwords do not match")])
     invite_code = StringField("Invite Code", validators=[DataRequired()])
     submit = SubmitField("Sign Up")
