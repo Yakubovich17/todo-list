a6ff8f6f066798bab31f30aa2fc176cf5120af44
Antorof1 <antorof@proton.me>
2025-01-18 19:12:08 +0100

    Added basic authentication

--- todolist/auth/models.py
+++ todolist/auth/models.py
@@ -1,14 +1,28 @@
 from flask_login import UserMixin
 
-from todolist import db
+from todolist import db, login_manager
 
 class User(db.Model, UserMixin):
-    __table_name__ = "users"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.LargeBinary)
 
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
     def __repr__(self):
         return f"<User {self.username}>"
+
+@login_manager.user_loader
+def user_loader(id):
+    return User.query.filter_by(id=id).first()
+
+@login_manager.request_loader
+def request_loader(request):
+    user = User.query.filter_by(username=request.form.get("username")).first()
+    return user if user else None
--- todolist/auth/routes.py
+++ todolist/auth/routes.py
@@ -1,7 +1,12 @@
+import os
+
 from flask import redirect, url_for, request, render_template
 
+from todolist import db
 from todolist.auth import blueprint
 from todolist.auth.forms import SignUpForm, SignInForm
+from todolist.auth.models import User
+from todolist.auth.utils import secure_password, verify_password
 
 @blueprint.route("/signin", methods=["GET", "POST"])
 def signin():
@@ -9,6 +14,19 @@ def signin():
 
     if request.method == "GET":
         return render_template("pages/signin.html", form=form)
+
+    if not form.validate_on_submit():
+        msg = next(iter(next(iter(form.errors.values()))), None)
+        return render_template("pages/signin.html", form=form, msg=msg)
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not verify_password(user.password, password):
+        return render_template("pages/signin.html", form=form, msg="Invalid username or password")
+
     return redirect(url_for("main.index"))
 
 @blueprint.route("/signup", methods=["GET", "POST"])
@@ -17,4 +35,26 @@ def signup():
 
     if request.method == "GET":
         return render_template("pages/signup.html", form=form)
+
+    if not form.validate_on_submit():
+        msg = next(iter(next(iter(form.errors.values()))), None)
+        return render_template("pages/signup.html", form=form, msg=msg)
+
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
+    invite_code = request.form["invite_code"]
+
+    if User.query.filter_by(username=username).first():
+        return render_template("pages/signup.html", form=form, msg="Username already registered")
+
+    if User.query.filter_by(email=email).first():
+        return render_template("pages/signup.html", form=form, msg="Email already registered")
+
+    if invite_code != os.environ.get("INVITE_CODE"):
+        return render_template("pages/signup.html", form=form, msg="Invite code is not valid")
+
+    db.session.add(User(username, email, secure_password(password)))
+    db.session.commit()
+
     return redirect(url_for("main.index"))
