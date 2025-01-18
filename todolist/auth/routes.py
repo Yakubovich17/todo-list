e6e18ce1bd4be3eb8a7d389c46102ea1bf7dbb5d
Antorof1 <antorof@proton.me>
2025-01-18 17:14:40 +0100

    Added user login/register form rendering

--- todolist/auth/routes.py
+++ todolist/auth/routes.py
@@ -1,11 +1,20 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, request, render_template
 
 from todolist.auth import blueprint
+from todolist.auth.forms import SignUpForm, SignInForm
 
 @blueprint.route("/signin", methods=["GET", "POST"])
 def signin():
+    form = SignInForm(request.form)
+
+    if request.method == "GET":
+        return render_template("pages/signin.html", form=form)
     return redirect(url_for("main.index"))
 
 @blueprint.route("/signup", methods=["GET", "POST"])
 def signup():
+    form = SignUpForm(request.form)
+
+    if request.method == "GET":
+        return render_template("pages/signup.html", form=form)
     return redirect(url_for("main.index"))
