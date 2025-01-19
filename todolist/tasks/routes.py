3260e5d36540164d057f8fd80264faec8db03bf0
Antorof1 <antorof@proton.me>
2025-01-19 15:28:23 +0100

    Added test user profile

--- todolist/tasks/routes.py
+++ todolist/tasks/routes.py
@@ -1,1 +1,9 @@
+from flask import render_template
+from flask_login.utils import login_required
+
 from todolist.tasks import blueprint
+
+@blueprint.route("/profile")
+@login_required
+def profile():
+    return render_template("pages/profile.html")
