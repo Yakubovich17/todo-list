9af22844772b1825007be31119762b7c5d05a623
Antorof1 <antorof@proton.me>
2025-01-19 16:00:57 +0100

    Added basic tasks render

--- todolist/tasks/routes.py
+++ todolist/tasks/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template
-from flask_login.utils import login_required
+from flask_login.utils import login_required, current_user
 
 from todolist.tasks import blueprint
 
 @blueprint.route("/profile")
 @login_required
 def profile():
-    return render_template("pages/profile.html")
+    return render_template("pages/profile.html", tasks=current_user.tasks)
