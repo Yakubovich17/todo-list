a7e097c84a7d6b5905437e38181779749de54620
Antorof1 <antorof@proton.me>
2025-01-19 17:13:39 +0100

    Added basic task creation

--- todolist/tasks/routes.py
+++ todolist/tasks/routes.py
@@ -1,9 +1,34 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login.utils import login_required, current_user
 
+from todolist import db
 from todolist.tasks import blueprint
+from todolist.tasks.models import Task
+from todolist.tasks.forms import TaskCreateForm
 
 @blueprint.route("/profile")
 @login_required
 def profile():
-    return render_template("pages/profile.html", tasks=current_user.tasks)
+    form = TaskCreateForm()
+
+    return render_template("pages/profile.html", tasks=current_user.tasks, form=form)
+
+@blueprint.route("/tasks", methods=["POST"])
+def create_task():
+    form = TaskCreateForm(request.form)
+
+    if not form.validate_on_submit():
+        return render_template("pages/profile.html", tasks=current_user.tasks, form=form)
+
+    title = request.form["title"]
+    description = request.form["description"]
+    user_id = current_user.id
+
+    print(description)
+
+    task = Task(title, description, user_id)
+
+    db.session.add(task)
+    db.session.commit()
+
+    return render_template("pages/profile.html", tasks=current_user.tasks, form=form)
