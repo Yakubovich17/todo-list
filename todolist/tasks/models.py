dda3d01ff658a80e597876602d237af38736aa8f
Antorof1 <antorof@proton.me>
2025-01-19 16:54:24 +0100

    Small Task model additions

--- todolist/tasks/models.py
+++ todolist/tasks/models.py
@@ -9,5 +9,19 @@ class Task(db.Model):
     completed = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
+    def __init__(self, title, description, user_id):
+        self.title = title
+        self.description = description
+        self.user_id = user_id
+
     def __repr__(self):
         return f"<Task {self.title}>"
+
+    def __todict__(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed": self.completed,
+            "user_id": self.user_id
+        }
