43349db604ef0b1118984b588d07db94e3b56e1c
Antorof1 <antorof@proton.me>
2025-01-20 17:26:32 +0100

    Added sample about us and contact pages. Added dynamic design for header

--- todolist/main/routes.py
+++ todolist/main/routes.py
@@ -4,4 +4,12 @@ from todolist.main import blueprint
 
 @blueprint.route("/")
 def index():
-    return render_template("pages/index.html")
+    return render_template("pages/index.html", current_page="home")
+
+@blueprint.route("/about")
+def about():
+    return render_template("pages/about.html", current_page="about")
+
+@blueprint.route("/contact")
+def contact():
+    return render_template("pages/contact.html", current_page="contact")
